18f247d6af96be6f1932c08ce07ba2320e847346
CAH1983 <carolineho1983@yahoo.fr>
2019-04-14 18:51:44 +0100

    whack-a-mole exercise

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -1,4 +1,4 @@
-const video = document.querySelector('.player');
+ const video = document.querySelector('.player');
 const canvas = document.querySelector('.photo');
 const ctx = canvas.getContext('2d');
 const strip = document.querySelector('.strip');
